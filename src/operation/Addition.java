b061d3025be4bedbbdbb1390a5fd823dcf059b87
Addryane Bautista <testing.testing4005@gmail.com>
2026-04-17 09:20:48 +0800

    feat: add calculator operation

---
+++ src/operation/Addition.java
@@ -0,0 +1,8 @@
+package operation;
+
+public class Addition implements Operation {
+    @Override
+    public double calculate(double num1, double num2) {
+        return num1 + num2;
+    }
+}
---
+++ src/operation/Operation.java
@@ -0,0 +1,5 @@
+package operation;
+
+public interface Operation {
+    double calculate(double num1, double num2);
+}
